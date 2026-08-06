b84d0718666b41f799797550d63a10b239a6611b
User <user@Masters-MacBook-Pro.local>
2026-08-06 22:39:56 +0500

    save: remove temporary reward key

--- server/server.js
+++ server/server.js
@@ -150,7 +150,6 @@ sockets.on('connection', socket => {
     if (message.type === 'eat' && [10, 25, 50].includes(message.value)) {
       player.score = Math.min(1_000_000, player.score + message.value);
     }
-    if (message.type === 'debugGrant') player.score = Math.min(1_000_000, player.score + 2000);
   });
 
   socket.on('close', () => {
